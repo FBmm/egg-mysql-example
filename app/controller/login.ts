9672e6688ed1eb7a42984d75dcc17d0d5d43c3b3
wq_pc <wuq_xd@163.com>
2020-03-10 00:15:26 +0800

    登陆接口controller

--- app/controller/login.ts
+++ app/controller/login.ts
@@ -3,10 +3,45 @@ import { print } from '../util';
 
 export default class LoginController extends Controller {
   public async login() {
-    const { ctx } = this;
     print.execute('LoginController');
-    ctx.body = await ctx.service.login.login();
-    ctx.body = 'login';
+    const { ctx } = this;
+    // 获取 http Request  body 参数
+    const { id } = ctx.request.body;
+    if (!id) {
+      ctx.body = {
+        code: 10001,
+        result: {
+          exception: '参数错误',
+          description: 'id is empty',
+          suggestion: ''
+        },
+        data: {},
+      }
+      return;
+    }
+    // 查询用户信息
+    const user = await ctx.service.login.queryUserById(id);
+    console.log(user);
+    if (user) {
+      ctx.body = {
+        code: 0,
+        result: {
+          exception: '',
+          description: '',
+          suggestion: ''
+        },
+        data: user,
+      }
+    } else {
+      ctx.body = {
+        code: 10000,
+        result: {
+          exception: '查询用户失败',
+          description: '',
+          suggestion: ''
+        },
+        data: {},
+      }
+    }
   }
-
 }
