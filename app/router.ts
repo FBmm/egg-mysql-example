f79fc302c31ca3fd88fcaef70f02950dd1a67103
wq_pc <wuq_xd@163.com>
2020-03-09 15:12:48 +0800

    增加 login 路由

--- app/router.ts
+++ app/router.ts
@@ -3,6 +3,8 @@ import { Application } from 'egg';
 export default (app: Application) => {
   const { controller, router } = app;
 
+  router.get('/', controller.home.index);
+  router.get('/login', controller.login.login);
   router.get('/find', controller.home.index);
   router.get('/insert', controller.home.insert);
 };
